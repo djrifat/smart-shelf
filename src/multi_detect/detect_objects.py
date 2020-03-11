2abd25811acce5cb1f97067695576168cc56af39
Daan <daniel.jrifat@gmail.com>
2020-03-11 14:45:59 +0100

    added new folder for demo's

--- src/multi_detect/detect_objects.py
+++ src/multi_detect/detect_objects.py
@@ -2,3 +2,8 @@
 
 
 # python train.py --logtostderr --train_dir=training/ --pipeline_config_path=training/ssd_mobilenet_v2_coco.config
+#from tensorflow.python.util import deprecation
+#deprecation._PRINT_DEPRECATION_WARNINGS = False
+
+
+
